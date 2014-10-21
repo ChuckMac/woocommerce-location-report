fc9213a10b981163b61db32ad8fa9879704d4e68
Chuck Mac <chuck@chuckmac.info>
2014-10-20 21:20:48 -0400

    Initial v1.0 release (!)

---
+++ readme.txt
@@ -0,0 +1,55 @@
+=== WooCommerce Sales By Location Report ===
+Contributors: chuckmac
+Tags: woocommerce, extension, reporting, analytics
+Requires at least: 3.8
+Tested up to: 4.0
+Stable tag: 1.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+WooCommerce reporting extension to elegantly display customer orders by location.
+
+== Description ==
+
+This plugin adds a new section in the WooCommerce Reports -> Orders area called 'Sales by location'.
+
+The report visualizes the customer purchases by location into a Choropleth map to show where your orders are being placed.
+
+* Display by shipping address or billing address
+* Display by number of orders or order amount.
+
+= Get involved =
+
+Developers can contribute to the source code on the [GitHub Repository](https://github.com/chuckmac/woocommerce-location-report).
+
+
+== Frequently Asked Questions ==
+
+= The sales report says I have more orders in the time period than the location report shows... why? =
+
+The location map only displays orders that have a recognizable address associated with the order.  For example, if there is no shipping address for the order then the map will not display the order if you are on the Shipping Address filter. 
+
+
+== Installation ==
+
+= Minimum Requirements =
+
+* WordPress 3.8 or greater
+* WooCommerce 2.2 or greater
+
+= Instructions =
+
+1. Upload the entire 'woocommerce-location-report' folder to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Screenshots ==
+
+1. Example number of order report map.
+2. Example sales volume report map.
+
+
+== Changelog ==
+
+= 1.0.0 - 10/15/2014 =
+* Initial Release (!)
---
+++ woocommerce-location-report.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Sales By Location Report
+ * Plugin URI: http://www.chuckmac.info
+ * Description: WooCommerce report to visualize sales by location.
+ * Author: Chuck Mac
+ * Author URI: http://www.chuckmac.info
+ * Version: 1.0.0
+ * Text Domain: wc_location_report
+ * Domain Path: /languages/
+ *
+ * Copyright: (c) 2014 ChuckMac Development LLC
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package   WC-Location-Report
+ * @author    WooThemes
+ * @category  Reports
+ * @copyright Copyright (c) 2014, ChuckMac Development LLC
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+// Check if WooCommerce is active
+if ( ! is_woocommerce_active() ) {
+	return;
+}
+
+
+/**
+ * # WooCommerce Location Report Main Plugin Class
+ *
+ * ## Plugin Overview
+ *
+ * This plugin adds a new section in the WooCommerce Reports -> Orders area called 'Sales by location'.
+ * The report visualizes the customer purchases by location into a Choropleth map to show where the orders
+ * are being placed.
+ *
+ * This plugin utilizes jVectorMap (http://jvectormap.com) for its map functions.
+ *
+ */
+class WC_Location_Report {
+
+	/** plugin version number */
+	public static $version = '1.0.0';
+
+	/** @var string the plugin file */
+	public static $plugin_file = __FILE__;
+
+	/** @var string the plugin file */
+	public static $plugin_dir;
+
+	
+	/**
+	 * Initializes the plugin
+	 *
+	 * @since 1.0
+	 */
+	public function init() {
+
+		global $wpdb;
+
+		self::$plugin_dir = dirname( __FILE__ );
+
+		// Add any necessary css / scripts
+		add_action( 'admin_enqueue_scripts', __CLASS__ . '::location_report_admin_css_scripts' );
+
+		// Add the reports layout to the WooCommerce -> Reports admin section
+		add_filter( 'woocommerce_admin_reports',  __CLASS__ . '::initialize_location_admin_report', 12, 1 );
+
+		// Add the path to the report class so WooCommerce can parse it
+		add_filter( 'wc_admin_reports_path',  __CLASS__ . '::initialize_location_admin_reports_path', 12, 3 );
+
+		// Load translation files
+		add_action( 'plugins_loaded', __CLASS__ . '::load_plugin_textdomain' );
+
+	}
+
+
+	/**
+	 * Add any location report javascript & css to the admin pages.  Only 
+	 * add it to our specific report areas.
+	 *
+	 * @since 1.0
+	 */
+	public static function location_report_admin_css_scripts() {
+
+		$wc_screen_id = sanitize_title( __( 'WooCommerce', 'woocommerce' ) );
+		$screen = get_current_screen();
+
+		if ( in_array( $screen->id, apply_filters( 'woocommerce_geo_reports_screen_ids', array( $wc_screen_id . '_page_wc-reports' ) ) ) && isset( $_REQUEST['report'] ) && in_array($_REQUEST['report'], apply_filters( 'woocommerce_geo_reports_report_ids', array( 'sales_by_location' ) )) ) {
+
+			//jVector includes - needs to be done in the footer so we can localize data as part of the report generation
+			wp_enqueue_script( 'jvectormap', plugins_url( '/lib/jquery-jvectormap-1.2.2.min.js', self::$plugin_file ), array( 'jquery' ), self::$version, true );
+			wp_enqueue_script( 'jvectormap-world', plugins_url( '/lib/map-data/jquery-jvectormap-world-mill-en', self::$plugin_file ), array( 'jquery', 'jvectormap' ), self::$version, true );
+
+			//jVector css
+			wp_enqueue_style( 'jvectormap', plugins_url( '/lib/jquery-jvectormap-1.2.2.css', self::$plugin_file ), array( 'woocommerce_admin_styles' ), self::$version );
+
+		}
+
+	}
+
+
+	/**
+	 * Add our location report to the WooCommerce order reports array.
+	 *
+	 * @param array Array of All Report types & their labels
+	 * @return array Array of All Report types & their labels, including the 'Sales by location' report.
+	 * @since 1.0
+	 */
+	public static function initialize_location_admin_report ( $report ) {
+
+		$report['orders']['reports']['sales_by_location'] = array (
+															'title'       => __( 'Sales by location', 'woocommerce-location-report' ),
+															'description' => '',
+															'hide_title'  => true,
+															'callback'    => array( 'WC_Admin_Reports', 'get_report' )
+															);
+
+		return $report;
+		
+	}
+
+
+	/**
+	 * If we hit one of our reports in the WC get_report function, change the path to our dir.
+	 *
+	 * @param array Array of Report types & their labels
+	 * @return array Array of Report types & their labels, including the Subscription product type.
+	 * @since 1.0
+	 */
+	public static function initialize_location_admin_reports_path( $report_path, $name, $class ) {
+
+		if ( 'WC_Report_sales_by_location' == $class ) {
+			$report_path = self::$plugin_dir . '/classes/class-wc-report-' . $name . '.php';
+		}
+		
+		return $report_path;
+
+	}
+
+
+	/**
+	 * Load our language settings for internationalization
+	 *
+	 * @since 1.0
+	 */
+	public static function load_plugin_textdomain( ) {
+
+		load_plugin_textdomain( 'woocommerce-location-report', false, basename( self::$plugin_dir ) . '/languages' );
+
+	}
+
+} // end \WC_Location_Report class
+
+
+WC_Location_Report::init();
